dce8c04d55f6633ef9d1ce7cfbf0a7b7ef1e4ce2
raju.sakinala <raju.sakinala@apollohl.com>
2024-12-19 23:25:19 +0530

    oiuodi

--- src/components/Counting.jsx
+++ src/components/Counting.jsx
@@ -3,32 +3,13 @@ import "./counting.css";
 import axios from "axios";
 
 const Counting = ({ setIsReady }) => {
+  // let baseURL = "http://localhost:1954";
+  let baseURL = "https://zphs-school.vercel.app";
   const [count, setCount] = useState(3); // Lunch countdown starts at 3
   const [isCounting, setIsCounting] = useState(false); // State to track lunch countdown
   const [timer, setTimer] = useState(true); // Track if target timer has expired
-  // Time left for the target countdown
-
-  // Function to calculate time left until the target time
-  // const calculateTimeLeft = () => {
-  //   const now = new Date();
-  //   const target = new Date();
-  //   target.setHours(22, 13, 0, 0); // Set target time to 5 PM
-
-  //   // If the target time has already passed today, set it for tomorrow
-  //   if (now > target) {
-  //     // target.setDate(target.getDate() + 1); // Change to +1 for tomorrow
-  //   }
-
-  //   const difference = target - now;
-  //   const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-  //   const hours = Math.floor(
-  //     (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  //   );
-  //   const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-  //   const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-
-  //   return { days, hours, minutes, seconds };
-  // };
+  const [time, setTime] = useState([11, 11]); // Track if target timer has expired
+  // let time = [11, 10]; // Track if target timer has expired
 
   const [timeLeft, setTimeLeft] = useState({
     days: 0,
@@ -37,11 +18,30 @@ const Counting = ({ setIsReady }) => {
     seconds: 0,
   });
   useEffect(() => {
+    axios
+      .get(`${baseURL}/time`)
+      .then((res) => {
+        setTime(res.data[0].time);
+        // time = res.data[0].time;
+        console.log("time get", res.data[0].time);
+      })
+      .catch((er) => {
+        console.log("error");
+      });
+  }, []);
+  useEffect(() => {
     const timer = setInterval(() => {
       const now = new Date();
       const target = new Date();
       target.setDate(target.getDate() + 2);
-      target.setHours(11, 0, 0, 0);
+      console.log("time", time);
+
+      target.setHours(
+        time.length > 0 ? time[0] : 10,
+        time.length > 0 ? time[1] : 10,
+        0,
+        0
+      );
 
       // target.setHours(22, 51, 0, 0);
 
@@ -70,7 +70,7 @@ const Counting = ({ setIsReady }) => {
       });
     }, 1000);
     return () => clearInterval(timer);
-  }, []);
+  }, [time]);
 
   // // Update `timeLeft` every second until target time is reached
   const handleLunchClick = () => {

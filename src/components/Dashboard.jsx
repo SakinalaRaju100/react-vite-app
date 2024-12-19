4bc5ca0381a5667c61c7ef50a50fee544c2c9a57
raju.sakinala <raju.sakinala@apollohl.com>
2024-12-19 23:43:34 +0530

    ads added

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -471,95 +471,148 @@ const adCarouselImages = [
   // },
 
   {
-    SNo: 1,
-    page: 1,
-    title: "ZPHS School",
-    image:
-      "https://drive.google.com/thumbnail?id=1-6hCnYvJG0XbWYKVrjoc7ca-Vd59SCUx&sz=w1000",
+    image: "https://neodesigns.in/zphs/ads/adv_1.jpg",
   },
   {
-    SNo: 2,
-    page: 2,
-    title: "Hiranmai Constructions And Precast Materials",
-    image:
-      "https://drive.google.com/thumbnail?id=1uwswwB8LFIsA3q9mTQUrMO5RY0csA4Ht&sz=w1000",
+    image: "https://neodesigns.in/zphs/ads/adv_2.jpg",
   },
   {
-    SNo: 3,
-    page: 4,
-    title: "Sai Bhavani motors And Goldnoon",
-    image:
-      "https://drive.google.com/thumbnail?id=1BNn7Ri0X-9kXJDEBreN97Bct_SHyUVGD&sz=w1000",
+    image: "https://neodesigns.in/zphs/ads/adv_3.jpg",
   },
   {
-    SNo: 3,
-    page: 8,
-    title: "Neelima Book Bank",
-    image:
-      "https://drive.google.com/thumbnail?id=1OTMpkCS52MoQhqUU6gAVgZy7G7IJ05HC&sz=w1000",
+    image: "https://neodesigns.in/zphs/ads/adv_4.jpg",
   },
   {
-    SNo: 3,
-    page: 12,
-    title: "Rudra Power Point",
-    image:
-      "https://drive.google.com/thumbnail?id=1gd1eyv53jOLoSVnp6eBDWWxhzHGJX9-u&sz=w1000",
+    image: "https://neodesigns.in/zphs/ads/adv_5.jpg",
   },
   {
-    SNo: 3,
-    page: 19,
-    title: "Geeta Auto Commercials",
-    image:
-      "https://drive.google.com/thumbnail?id=1eYrnqFB_zvFZFbqdGWVBuqxci3KZnTl_&sz=w1000",
+    image: "https://neodesigns.in/zphs/ads/adv_6.jpg",
   },
   {
-    SNo: 3,
-    page: 22,
-    title: "Anjana Chit Funds",
-    image:
-      "https://drive.google.com/thumbnail?id=1ks4BD6a4FtgwqLL9Me-YzQlpdV66q_IX&sz=w1000",
+    image: "https://neodesigns.in/zphs/ads/adv_7.jpg",
   },
   {
-    SNo: 3,
-    page: 24,
-    title: "ZPHS Principals",
-    image:
-      "https://drive.google.com/thumbnail?id=1U5KimFJeKyc8gHMFTIFSKP4FeQoUTVAn&sz=w1000",
+    image: "https://neodesigns.in/zphs/ads/adv_8.jpg",
   },
   {
-    SNo: 3,
-    page: 27,
-    title: "Allphores IIT & NEET Academy",
-    image:
-      "https://drive.google.com/thumbnail?id=1zYYvYMmbvGF0sLQriIoE5RRnrvffz-8P&sz=w1000",
+    image: "https://neodesigns.in/zphs/ads/adv_9.jpg",
   },
   {
-    SNo: 3,
-    page: 29,
-    title: "Natural & Pure Oils",
-    image:
-      "https://drive.google.com/thumbnail?id=1mGvqa9ffjJ0FoxRY91g3StcjiIp4zl3E&sz=w1000",
+    image: "https://neodesigns.in/zphs/ads/adv_10.jpg",
   },
   {
-    SNo: 3,
-    page: 35,
-    title: "Annapurna Seeds & Pesticides",
-    image:
-      "https://drive.google.com/thumbnail?id=1jhBS7tfcEw5uvKi_rgzIYVw-vYTaUgiT&sz=w1000",
+    image: "https://neodesigns.in/zphs/ads/adv_11.jpg",
   },
   {
-    SNo: 3,
-    page: 39,
-    title: "CIVILDEN Green infra pvt.ltd",
-    image:
-      "https://drive.google.com/thumbnail?id=1jQb4C1pyG8QXCwtl4w4aTrTXaitBZ4Ot&sz=w1000",
+    image: "https://neodesigns.in/zphs/ads/adv_12.jpg",
   },
   {
-    SNo: 3,
-    page: 42,
-    title: "Cable tech Industries",
-    image:
-      "https://drive.google.com/thumbnail?id=1eCr3FbNPwhE8WCGjXTybCZnhmFDsiCeq&sz=w1000",
+    image: "https://neodesigns.in/zphs/ads/adv_13.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_14.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_15.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_16.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_17.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_18.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_19.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_20.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_21.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_21.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_22.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_23.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_24.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_25.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_26.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_27.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_28.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_29.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_30.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_31.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_32.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_33.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_34.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_35.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_36.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_37.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_38.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_39.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_40.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_41.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_42.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_43.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_44.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_45.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_46.jpg",
+  },
+  {
+    image: "https://neodesigns.in/zphs/ads/adv_47.jpg",
   },
 ];
 
@@ -610,9 +663,9 @@ const AdCarousel = () => {
             color: "white",
           }}
         >
-          <Typography variant="h6">
+          {/* <Typography variant="h6">
             {adCarouselImages[adCarouselIndex].title}
-          </Typography>
+          </Typography> */}
           {/*<Typography variant="body2">
               {adCarouselImages[adCarouselIndex].description}
             </Typography>*/}
@@ -637,7 +690,7 @@ const AdCarousel = () => {
           }
           sx={{
             position: "absolute",
-            left: 10,
+            left: 6,
             top: "50%",
             transform: "translateY(-50%)",
           }}
@@ -652,7 +705,7 @@ const AdCarousel = () => {
           }
           sx={{
             position: "absolute",
-            right: 10,
+            right: 6,
             top: "50%",
             transform: "translateY(-50%)",
           }}

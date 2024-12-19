083b3aad3057e279094f65d6cc12b3d30bfe73d9
raju.sakinala <raju.sakinala@apollohl.com>
2024-12-19 23:51:40 +0530

    added ads titles

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1886,7 +1886,7 @@ const Dashboard = () => {
         {/* Hero Section with Carousel */}
         <Box
           style={{
-            fontSize: "8px",
+            fontSize: "11px",
             backgroundColor: "#b388ff",
             color: "white",
             // "&:hover": {
@@ -1895,7 +1895,32 @@ const Dashboard = () => {
           }}
         >
           <marquee behavior="scroll" direction="left">
-            Here all advertisements scroll.
+            zphs school & Hiranmai Constructions And PrecastMaterials & Sai
+            Bhavani motors And Goldnoon & Neelima Book Bank & Rudra Power Point
+            & Geeta Auto Commercials Anjana Chit Funds & ZPHS Principals &
+            Allphores IIT & NEET Academy & Natural & Pure Oils & Annapurna Seeds
+            & Pesticides & CIVILDEN Green infra pvt.ltd & Cable tech industries
+            & Birla Open Minds & Adithya Industry & LIC of India Akula
+            Kumaraswamy & Muthya Kidney Centre & Kunur Temples & LOLA live a
+            Pleasant Life & Kaveri Borewells & Krishna Fertilizers Deport & LIC
+            Mamidala Srinivas & Lavanya Furnishings & NTIPL & V S Groups & Thota
+            Butchilingam Anaj & General Merchants & Prasanna Agencies & Sri
+            Vishwakarma Plywoods & RajuParusharamulu SLV ENERTECH Solutions &
+            Sri Sainath Good Carrier & Neeling Book Links & Dhanalaxmi Matchings
+            GNP & Padhmavati Conventions & Balaji Iron Hardwares & My Paisaa &
+            Kusuma Industries & Sriraksha Dental, Ent, Homoro Clinic & Legal
+            Adept & Special Class civil Contractor T.Baskar Rao & Padhmavati
+            rice & Dry fruits & Srinivasu Jewellery & Mahadev Opticals &
+            Suthri.Ashok Agro Trading & Dr.P Srinivas & Pragathi Graphics &
+            Someswara Stores & Sri Balaji Electronics & Home Needs & Sri
+            Venkateshwara Associates & B.Govindharajan, M.Subhash, Mallaiah,
+            P.Narothham reddy & Sri Donthi.Madhavareddy Narsampet & Sri
+            Yerrabelly.Dhayakar Rao & Jagruthi Creations & Arya Samaj Uppal &
+            Nomula.Ravindar & Y.Subhashini & Mahajan Garments & Belli.Jalender
+            Yadav & Pattipaka.Laxman S/o Laxminarsu & Arya Samaj Uppal & Ravi
+            Enterprises & Madhu Electricals, Winding Works & Ayyappa Seeds
+            Agencies & B.Bikshapati & Dr.Pitta Sathyam & T. Ravinder & denim
+            mart &
           </marquee>
         </Box>
         <Box
